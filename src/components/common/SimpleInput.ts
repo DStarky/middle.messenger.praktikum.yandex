f38d492cecd309d030607f31c38898c52ada83d1
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-12-01 15:51:00 +0500

    feat: добавил базовые компоненты для инпутов

---
+++ src/components/common/SimpleInput.ts
@@ -0,0 +1,11 @@
+export const SimpleInput = `
+<div class="simple-input-container">
+  <input 
+    type="{{type}}" 
+    id="{{id}}" 
+    name="{{name}}" 
+    class="simple-input {{className}}" 
+    placeholder="{{placeholder}}" 
+    value="{{value}}" 
+  />
+</div>`;
