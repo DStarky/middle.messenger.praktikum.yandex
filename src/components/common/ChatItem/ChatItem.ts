f97a13d1205640f3dcbabff8181719ac1da44a7f
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-12-02 08:06:45 +0500

    feat: добавил компонент CardItem

---
+++ src/components/common/ChatItem/ChatItem.ts
@@ -0,0 +1,17 @@
+export const ChatItem = `
+<div class="chat-item-container">
+  <div class="chat-item__avatar">
+    {{> Avatar src=chat.avatar alt=chat.name}}
+  </div>
+  <div class="chat-item__content">
+    <div class="chat-item__name">{{chat.name}}</div>
+    <div class="chat-item__last-message">{{chat.lastMessage}}</div>
+  </div>
+  <div class="chat-item__meta">
+    <div class="chat-item__time">{{chat.time}}</div>
+    {{#if chat.unreadCount}}
+      <div class="chat-item__unread-count">{{chat.unreadCount}}</div>
+    {{/if}}
+  </div>
+</div>
+`;
