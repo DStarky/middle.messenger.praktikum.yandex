ee3475494a5b93ffd06af618a52d0f723dc83667
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-12-03 19:32:33 +0500

    fix: починил перерендер в ChatPage

--- src/pages/ChatsPage/ChatsPage.ts
+++ src/pages/ChatsPage/ChatsPage.ts
@@ -1,3 +1,4 @@
+import Handlebars from 'handlebars';
 import { BasePage } from '../basePage';
 import MenuIcon from '../../assets/icons/menu.svg';
 import AttachmentIcon from '../../assets/icons/attachment.svg';
@@ -87,7 +88,56 @@ export class ChatsPage extends BasePage {
       messages: selectedChat ? this.getMessages(selectedChat.id) : [],
     };
 
-    document.body.innerHTML = this.render(context);
+    const sidebarElement = document.querySelector('.chats-page__sidebar');
+    const chatContentElement = document.querySelector('.chat-content');
+
+    if (sidebarElement && chatContentElement) {
+      sidebarElement.innerHTML = this.renderSidebar(context);
+      chatContentElement.innerHTML = this.renderChatContent(context);
+    }
+  }
+
+  private renderSidebar(context: Record<string, unknown>): string {
+    return Handlebars.compile(`
+    {{> Sidebar chats=chats}}
+  `)(context);
+  }
+
+  private renderChatContent(context: Record<string, unknown>): string {
+    return Handlebars.compile(`
+    {{#if selectedChat}}
+      <div class="chat-header">
+        {{> Avatar src=selectedChat.avatar alt=selectedChat.name className="avatar_size-small"}}
+        <div class="chat-header__name">{{selectedChat.name}}</div>
+        <button class="chat-header__settings">
+          <img src="${MenuIcon}" alt="menu" />
+        </button>
+      </div>
+      <div class="chat-messages">
+        {{#each messages}}
+          {{> Message message=this}}
+        {{/each}}
+      </div>
+      <div class="chat-input">
+        <button class="chat-input__attach">
+          <img src="${AttachmentIcon}" alt="attach" />
+        </button>
+        <div class="chat-input__message">
+          {{> SimpleInput type="text" id="message" name="message" placeholder="Сообщение" className="simple-input_message"}}
+        </div>
+        {{> Button 
+          type="button" 
+          className="button_round" 
+          icon="${ArrowRightIcon}" 
+          alt="Send"
+        }}
+      </div>
+    {{else}}
+      <div class="no-chat-selected">
+        Выберите чат, чтобы отправить сообщение
+      </div>
+    {{/if}}
+  `)(context);
   }
 
   private getChats(): Chat[] {
