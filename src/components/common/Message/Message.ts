c04b084b422e459e6cb2f208e40593200119730c
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-12-03 08:04:32 +0500

    style: стилизовал вывод сообщений - базовые стили

--- src/components/common/Message/Message.ts
+++ src/components/common/Message/Message.ts
@@ -1,5 +1,5 @@
 export const Message = `
-<div class="message {{#if message.isOwn}}message--own{{/if}}">
+<div class="message {{#if message.isOwn}}message_own{{else}}message_other{{/if}}">
   <div class="message__content">{{message.text}}</div>
   <div class="message__time">{{message.time}}</div>
 </div>
