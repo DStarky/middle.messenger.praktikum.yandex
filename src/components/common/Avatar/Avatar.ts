7d47f81c707a9ab2d28014df52452f2278f85d6a
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-12-03 07:34:12 +0500

    feat: добавил размеры для Avatar

--- src/components/common/Avatar/Avatar.ts
+++ src/components/common/Avatar/Avatar.ts
@@ -1,5 +1,5 @@
 export const Avatar = `
   <div class="avatar">
-    <img src="{{defaultSrc src}}" alt="{{alt}}" class="avatar-img" />
+    <img src="{{defaultSrc src}}" alt="{{alt}}" class="avatar-img {{className}}" />
   </div>
 `;
