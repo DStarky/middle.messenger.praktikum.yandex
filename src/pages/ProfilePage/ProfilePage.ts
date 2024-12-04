2b66d98f7803510133bc6fae0309b80742e1cb7e
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-12-04 06:45:23 +0500

    feat: добавил общую верстку компонента ProfilePage

--- src/pages/ProfilePage/ProfilePage.ts
+++ src/pages/ProfilePage/ProfilePage.ts
@@ -1,3 +1,4 @@
+import { ROUTES } from '../../app/routes';
 import { BasePage } from '../basePage';
 
 const template = `
@@ -5,9 +6,53 @@ const template = `
   <div class="profile-page__sidebar">
     {{> Sidebar compact=true}}
   </div>
-  <section class="profile-content">
-    <div class="profile-placeholder">
-      Тут будет профиль
+  <section class="profile-page__content">
+    <div class="profile-page__data">
+      <div class="profile-page__block">
+        <div class="profile-page__avatar">
+          {{> Avatar src='' alt='' class="avatar_size-large"}}
+        </div>
+        <h4 class="profile-page__name">Иван</h4>
+      </div>
+      <div class="profile-page__block">
+        <div class="profile-page__item">
+          <p class="profile-page__left">Почта</p>
+          <p class="profile-page__right">pochta@yandex.ru</p>
+        </div>
+        <div class="profile-page__item">
+          <p class="profile-page__left">Логин</p>
+          <p class="profile-page__right">ivanivanov</p>
+        </div>
+        <div class="profile-page__item">
+          <p class="profile-page__left">Имя</p>
+          <p class="profile-page__right">Иван</p>
+        </div>
+        <div class="profile-page__item">
+          <p class="profile-page__left">Фамилия</p>
+          <p class="profile-page__right">Иванов</p>
+        </div>
+        <div class="profile-page__item">
+          <p class="profile-page__left">Имя в чате</p>
+          <p class="profile-page__right">Иван</p>
+        </div>
+        <div class="profile-page__item">
+          <p class="profile-page__left">Телефон</p>
+          <p class="profile-page__right">+7 (909) 967 30 30</p>
+        </div>
+      </div>
+
+      <div class="profile-page__block">
+        <div class="profile-page__item">
+          {{> Link href="#" text="Изменить данные" className="profile-page__edit-link"}}
+        </div>
+        <div class="profile-page__item">
+          {{> Link href="#" text="Изменить пароль" className="profile-page__edit-link"}}
+        </div>
+        <div class="profile-page__item">
+          {{> Link href="${ROUTES.LOGIN}" text="Выйти" className="profile-page__logout-link"}}
+        </div>
+      </div>
+  
     </div>
   </section>
 </main>
