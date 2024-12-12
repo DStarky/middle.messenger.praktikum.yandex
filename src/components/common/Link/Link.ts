2ada1d39066f219d6f897d550c5d8ad1a93e91fe
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-12-12 16:13:00 +0500

    feat: обновил компонент Link

--- src/components/common/Link/Link.ts
+++ src/components/common/Link/Link.ts
@@ -1,2 +1,17 @@
-export const Link = `
-  <a href="{{href}}" class="default-link {{className}}">{{text}}</a>`;
+import { Block } from '../../../app/Block';
+
+export class Link extends Block {
+  constructor(props: {
+    href: string;
+    text: string;
+    className?: string;
+    events?: Record<string, (e: Event) => void>;
+  }) {
+    super('a', props);
+  }
+
+  override render() {
+    const template = `<a href="{{href}}" class="default-link {{className}}">{{text}}</a>`;
+    return this.compile(template, this.props);
+  }
+}
