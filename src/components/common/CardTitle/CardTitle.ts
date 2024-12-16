47c8ef01838fd835cb684f18885a0f815a404739
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-12-16 17:36:25 +0500

    feat: переписал компонент CardTitle, используя родительский класс Block

--- src/components/common/CardTitle/CardTitle.ts
+++ src/components/common/CardTitle/CardTitle.ts
@@ -1,3 +1,21 @@
-export const CardTitle = `
+import { Block } from '../../../app/Block';
+
+export const template = `
   <h2 class="card-title">{{text}}</h2>
 `;
+
+type CardTitleProps = {
+  text: string;
+  className?: string;
+  events?: Record<string, (e: Event) => void>;
+};
+
+export class CardTitle extends Block {
+  constructor(props: CardTitleProps) {
+    super('h2', props);
+  }
+
+  override render(): DocumentFragment {
+    return this.compile(template, this.props);
+  }
+}
