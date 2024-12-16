a907dd98969426355ae96d77cd95c35d324ce25d
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-12-16 17:36:05 +0500

    feat: переписал компонент Avatar, используя родительский класс Block

--- src/components/common/Avatar/Avatar.ts
+++ src/components/common/Avatar/Avatar.ts
@@ -1,5 +1,29 @@
-export const Avatar = `
+import { Block } from '../../../app/Block';
+import Handlebars from 'handlebars';
+
+Handlebars.registerHelper('defaultSrc', function (src: string) {
+  return src || 'https://default-avatar.com/default.jpg';
+});
+
+const AvatarTemplate = `
   <div class="avatar">
     <img src="{{defaultSrc src}}" alt="{{alt}}" class="avatar-img {{className}}" />
   </div>
 `;
+
+type AvatarProps = {
+  src: string;
+  alt: string;
+  className?: string;
+  events?: Record<string, (e: Event) => void>;
+};
+
+export class Avatar extends Block {
+  constructor(props: AvatarProps) {
+    super('div', props);
+  }
+
+  override render(): DocumentFragment {
+    return this.compile(AvatarTemplate, this.props);
+  }
+}
