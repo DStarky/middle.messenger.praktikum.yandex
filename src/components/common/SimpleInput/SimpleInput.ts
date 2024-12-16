02f319e8a03d79cc0fc7b0f4d1b4b325b83213a4
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-12-16 17:37:34 +0500

    feat: переписал компонент SimpleInput, используя родительский класс Block

--- src/components/common/SimpleInput/SimpleInput.ts
+++ src/components/common/SimpleInput/SimpleInput.ts
@@ -1,6 +1,7 @@
+import { Block } from '../../../app/Block';
 import SearchIcon from '../../../assets/icons/search.svg';
 
-export const SimpleInput = `
+export const template = `
 <div class="simple-input-container">
   <input 
     type="{{type}}" 
@@ -14,3 +15,24 @@ export const SimpleInput = `
       <img src="${SearchIcon}" alt="search" class="simple-input_icon" />
   {{/if}}
 </div>`;
+
+type SimpleInputProps = {
+  type: string;
+  id: string;
+  name: string;
+  placeholder: string;
+  value?: string;
+  className?: string;
+  search?: boolean;
+  events?: Record<string, (e: Event) => void>;
+};
+
+export class SimpleInput extends Block {
+  constructor(props: SimpleInputProps) {
+    super('div', props);
+  }
+
+  override render(): DocumentFragment {
+    return this.compile(template, this.props);
+  }
+}
