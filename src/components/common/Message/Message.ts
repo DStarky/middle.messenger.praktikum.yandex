b32cd053ae5ec2cd8cd3473948d373eb7809ad92
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-12-16 17:37:08 +0500

    feat: переписал компонент Message, используя родительский класс Block

--- src/components/common/Message/Message.ts
+++ src/components/common/Message/Message.ts
@@ -1,6 +1,31 @@
-export const Message = `
+import { Block } from '../../../app/Block';
+
+export const template = `
 <div class="message {{#if message.isOwn}}message_own{{else}}message_other{{/if}}">
   <div class="message__content">{{message.text}}</div>
   <div class="message__time">{{message.time}}</div>
 </div>
 `;
+
+type MessageData = {
+  id: string;
+  text: string;
+  time: string;
+  isOwn?: boolean;
+};
+
+type MessageProps = {
+  message: MessageData;
+  className?: string;
+  events?: Record<string, (e: Event) => void>;
+};
+
+export class Message extends Block {
+  constructor(props: MessageProps) {
+    super('div', props);
+  }
+
+  override render(): DocumentFragment {
+    return this.compile(template, this.props);
+  }
+}
