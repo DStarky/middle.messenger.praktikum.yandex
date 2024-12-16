ffc0de7418688d262dd10f7b903e1489d3f38959
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-12-16 17:36:15 +0500

    feat: переписал компонент Button, используя родительский класс Block

--- src/components/common/Button/Button.ts
+++ src/components/common/Button/Button.ts
@@ -1,10 +1,32 @@
-export const Button = `
-<button 
-  type="{{type}}" 
-  class="button {{className}}">
-  {{#if icon}}
-    <img src="{{icon}}" alt="{{alt}}" class="button__icon" />
-  {{else}}
-    {{text}}
-  {{/if}}
-</button>`;
+import { Block } from '../../../app/Block';
+
+const ButtonTemplate = `
+  <button 
+    type="{{type}}" 
+    class="button {{className}}">
+    {{#if icon}}
+      <img src="{{icon}}" alt="{{alt}}" class="button__icon" />
+    {{else}}
+      {{text}}
+    {{/if}}
+  </button>
+`;
+
+type ButtonProps = {
+  type: string;
+  className?: string;
+  icon?: string;
+  alt?: string;
+  text?: string;
+  events?: Record<string, (e: Event) => void>;
+};
+
+export class Button extends Block {
+  constructor(props: ButtonProps) {
+    super('button', props);
+  }
+
+  override render(): DocumentFragment {
+    return this.compile(ButtonTemplate, this.props);
+  }
+}
