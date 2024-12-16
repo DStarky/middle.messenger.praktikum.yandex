aa84c422e8b78c1aad160e5ff052ec6d75fa4d36
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-12-16 19:48:09 +0500

    feat: обновил App под новые компоненты и страницы

--- src/app/App.ts
+++ src/app/App.ts
@@ -3,6 +3,7 @@ import { Router } from './Router';
 
 import { Page404 } from '../pages/404/Page404';
 import { Page500 } from '../pages/500/Page500';
+import { LoginPage } from '../pages/LoginPage/LoginPage';
 
 export class App {
   private router: Router;
@@ -17,6 +18,11 @@ export class App {
 
     this.router.addRoute(ROUTES.ERROR_500, () => {
       const page = new Page500();
+      this.router.render(page.getContent()!);
+    });
+
+    this.router.addRoute(ROUTES.LOGIN, () => {
+      const page = new LoginPage(this.router);
       this.router.render(page.getContent()!);
     });
 
