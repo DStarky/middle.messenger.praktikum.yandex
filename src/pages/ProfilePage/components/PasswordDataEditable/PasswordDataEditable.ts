6218c5f32f8acbb32912f1eae17a0c710491e771
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-12-19 23:03:19 +0500

    feat: добавил валидацию для PasswordDataEditable

--- src/pages/ProfilePage/components/PasswordDataEditable/PasswordDataEditable.ts
+++ src/pages/ProfilePage/components/PasswordDataEditable/PasswordDataEditable.ts
@@ -24,6 +24,16 @@ export class PasswordDataEditable extends Block<Props> {
         events: props.events?.oldPasswordChange
           ? { change: props.events.oldPasswordChange }
           : undefined,
+        validationRules: [
+          {
+            validator: (value: string) => value.trim().length > 0,
+            message: 'Старый пароль обязателен для заполнения',
+          },
+          {
+            validator: (value: string) => value.length >= 6,
+            message: 'Пароль должен содержать не менее 6 символов',
+          },
+        ],
       }),
       newPassword: new ProfileEditableField({
         id: 'new-password-input',
@@ -35,6 +45,25 @@ export class PasswordDataEditable extends Block<Props> {
         events: props.events?.newPasswordChange
           ? { change: props.events.newPasswordChange }
           : undefined,
+        validationRules: [
+          {
+            validator: (value: string) => value.trim().length > 0,
+            message: 'Новый пароль обязателен для заполнения',
+          },
+          {
+            validator: (value: string) => value.length >= 8,
+            message: 'Пароль должен содержать не менее 8 символов',
+          },
+          {
+            validator: (value: string) =>
+              /[A-Z]/.test(value) &&
+              /[a-z]/.test(value) &&
+              /[0-9]/.test(value) &&
+              /[\W_]/.test(value),
+            message:
+              'Пароль должен содержать хотя бы одну заглавную букву, одну строчную букву, одну цифру и один специальный символ',
+          },
+        ],
       }),
       confirmPassword: new ProfileEditableField({
         id: 'confirm-password-input',
@@ -46,6 +75,16 @@ export class PasswordDataEditable extends Block<Props> {
         events: props.events?.confirmPasswordChange
           ? { change: props.events.confirmPasswordChange }
           : undefined,
+        validationRules: [
+          {
+            validator: (value: string) => value.trim().length > 0,
+            message: 'Подтверждение пароля обязательно для заполнения',
+          },
+          {
+            validator: (value: string) => value === props.newPasswordValue,
+            message: 'Пароли не совпадают',
+          },
+        ],
       }),
     });
   }
