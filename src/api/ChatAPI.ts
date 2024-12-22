49229f5993f1039a44965d55faccb95d4da7193d
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-12-22 22:41:13 +0500

    feat: убрал вероятность ошибки при отправке сообщения

--- src/api/ChatAPI.ts
+++ src/api/ChatAPI.ts
@@ -4,31 +4,21 @@ import { DEFAULT_CHATS, MESSAGES_BY_CHAT_ID } from '../consts/data';
 
 export class ChatAPI {
   public static fetchChats(): Promise<Chat[]> {
-    return new Promise((resolve, reject) => {
+    return new Promise(resolve => {
       setTimeout(() => {
-        const shouldError = Math.random() < 0.2;
-        if (shouldError) {
-          reject(new Error('Не удалось загрузить чаты.'));
-        } else {
-          resolve(DEFAULT_CHATS);
-        }
+        resolve(DEFAULT_CHATS);
       }, 500);
     });
   }
 
   public static fetchMessages(chatId: string): Promise<MessageData[]> {
-    return new Promise((resolve, reject) => {
+    return new Promise(resolve => {
       setTimeout(() => {
-        const shouldError = Math.random() < 0.1;
-        if (shouldError) {
-          reject(new Error('Не удалось загрузить сообщения.'));
+        const messages = MESSAGES_BY_CHAT_ID[chatId];
+        if (messages) {
+          resolve(messages);
         } else {
-          const messages = MESSAGES_BY_CHAT_ID[chatId];
-          if (messages) {
-            resolve(messages);
-          } else {
-            resolve([]);
-          }
+          resolve([]);
         }
       }, 500);
     });
@@ -38,19 +28,14 @@ export class ChatAPI {
     chatId: string,
     message: MessageData,
   ): Promise<void> {
-    return new Promise((resolve, reject) => {
+    return new Promise(resolve => {
       setTimeout(() => {
-        const shouldError = Math.random() < 0.1;
-        if (shouldError) {
-          reject(new Error('Не удалось отправить сообщение.'));
-        } else {
-          if (!MESSAGES_BY_CHAT_ID[chatId]) {
-            MESSAGES_BY_CHAT_ID[chatId] = [];
-          }
-
-          MESSAGES_BY_CHAT_ID[chatId].push(message);
-          resolve();
+        if (!MESSAGES_BY_CHAT_ID[chatId]) {
+          MESSAGES_BY_CHAT_ID[chatId] = [];
         }
+
+        MESSAGES_BY_CHAT_ID[chatId].push(message);
+        resolve();
       }, 500);
     });
   }
